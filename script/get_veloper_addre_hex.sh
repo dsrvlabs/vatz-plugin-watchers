8a69dd4e84371ea5cc1a12bc962857909ff716c4
xellos00 <xellos.kang@gmail.com>
2024-07-21 22:34:34 -0500

    - add pagination function in script

--- script/get_veloper_addre_hex.sh
+++ script/get_veloper_addre_hex.sh
@@ -3,25 +3,20 @@
 # Function to prompt for user input
 prompt_for_input() {
     local prompt_message=$1
-    local input_variable
     read -p "$prompt_message: " input_variable
     echo $input_variable
 }
 
-# Prompt for RPC address
+# Prompt for necessary inputs
 REST_END_POINT=$(prompt_for_input "Enter the Rest Endpoint")
-# Prompt for endpoint
 RPC_END_POINT=$(prompt_for_input "Enter the RPC Endpoint")
-# Prompt for valoper address
 VALOPER_ADDR=$(prompt_for_input "Enter the valoper address")
 
-# Display the entered values and ask for confirmation
-echo " "
-echo "You have entered the following details:"
+# Display entered values and ask for confirmation
+echo -e "\nYou have entered the following details:"
 echo "Rest Endpoint: $REST_END_POINT"
 echo "RPC Endpoint: $RPC_END_POINT"
 echo "Valoper(Validator Operator) address: $VALOPER_ADDR"
-
 read -p "Are the entered values correct? Do you wish to proceed? (y/n): " confirmation
 confirmation=$(echo "$confirmation" | tr '[:upper:]' '[:lower:]')
 
@@ -33,21 +28,47 @@ fi
 # Fetch the consensus public key
 CONS_PUB_KEY=$(curl -s "${REST_END_POINT}/cosmos/staking/v1beta1/validators/${VALOPER_ADDR}" | jq -r '.validator.consensus_pubkey.key')
 
-# Check if the consensus public key was fetched successfully
 if [ -z "$CONS_PUB_KEY" ]; then
     echo "Failed to fetch the consensus public key."
     exit 1
 fi
 
-# Fetch the validator address using the consensus public key
-VALIDATOR_ADDR=$(curl -s "${RPC_END_POINT}/validators" | jq -r --arg value "$CONS_PUB_KEY" '.result.validators[] | select(.pub_key.value == $value) | .address')
+echo "Consensus public key: $CONS_PUB_KEY"
 
+# Function to fetch validators from a specific page
+fetch_validators() {
+    local page=$1
+    curl -s "${RPC_END_POINT}/validators?page=${page}" | jq -r '.result.validators[]'
+}
 
-# Check if the validator address was fetched successfully
+# Initialize variables for pagination
+PAGE=1
+VALIDATOR_ADDR=""
+
+# Loop through pages to find the validator address
+while : ; do
+    VALIDATORS=$(fetch_validators $PAGE)
+
+    # If no validators are returned, break the loop
+    if [ -z "$VALIDATORS" ]; then
+        break
+    fi
+
+    # Check if the desired validator is in the current page
+    VALIDATOR_ADDR=$(echo "$VALIDATORS" | jq -r --arg value "$CONS_PUB_KEY" 'select(.pub_key.value == $value) | .address')
+
+    # If the validator address is found, break the loop
+    if [ -n "$VALIDATOR_ADDR" ]; then
+        break
+    fi
+
+    # Move to the next page
+    PAGE=$((PAGE + 1))
+done
+
 if [ -z "$VALIDATOR_ADDR" ]; then
     echo "Failed to fetch the validator hex address."
     exit 1
 fi
 
-# Output the final validator address
 echo "Validator hex address: $VALIDATOR_ADDR"
